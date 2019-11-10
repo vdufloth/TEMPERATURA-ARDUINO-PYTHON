b38939d371988f37078634ef03a00864a174a02d
vinidufloth <vinicius@cyberlog.net>
2019-11-10 09:35:53 -0300

    url exemplo

--- python/ArduinoInterface.py
+++ python/ArduinoInterface.py
@@ -2,10 +2,12 @@ from threading import Thread
 import serial
 from datetime import datetime
 import requests
+import json
 
 ARDUINO_LOCATION = '/dev/ttyACM0'
 SERIAL_PORT = 9600
-POST_URL = ''
+POST_URL = 'http://192.168.0.99/sist-distribuidos/api/data.php'
+
 
 class SerialReader(Thread):
     def __init__(self):
@@ -31,11 +33,11 @@ class SerialReader(Thread):
             if self.ser.isOpen():
                 lineRead = self.ser.readline().decode("utf-8").split(sep=";")
                 time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                register = {'hora': time,
-                            'umidade': lineRead[0],
-                            'temperatura': lineRead[1],
-                            }
-                print('Enviando', register, 'para',POST_URL)
+                register = json.dumps({'readtime': time,
+                                       'humidity': lineRead[0],
+                                       'temperature': lineRead[1],
+                                       })
+                print('Enviando', register, 'para', POST_URL)
                 try:
                     result = requests.post(POST_URL, data=register)
                     print('Post result:', result.text)
@@ -43,15 +45,15 @@ class SerialReader(Thread):
                     print('Erro ao enviar:', error)
             else:
                 print('Porta serial nao aberta:', datetime.now())
-    
+
     def stopReading(self):
         print('Parando leitura')
         self.keepReading = False
-    
+
     def restartReading(self):
         print('Reiniciando leitura')
         self.keepReading = True
 
     def setReadingInterval(self, seconds):
         print('Alterando intervalo de leitura no Arduino para', seconds, 'segundos')
-        #Pass to arduino
+        # Pass to arduino
